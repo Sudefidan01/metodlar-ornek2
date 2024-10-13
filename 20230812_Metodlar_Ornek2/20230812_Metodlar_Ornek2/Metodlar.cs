7f331399ca4fa6ed328e8f643d00011fd953678b
sude <sudefidan@icloud.com>
2024-10-13 18:16:49 +0300

    metodlar-ornek2

---
+++ 20230812_Metodlar_Ornek2/20230812_Metodlar_Ornek2/Metodlar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20230812_Metodlar_Ornek2
+{
+    internal class Metodlar
+    {
+        public int ToplamaYaptir(int[] sayilar)
+        {
+            int toplam = 0;
+            foreach (var item in sayilar)
+            {
+                toplam += item;
+            }
+            return toplam;
+        }
+
+        public void ToplamiVeOrtalamayiYazdir(int toplam,int adet)
+        {
+            Console.WriteLine("Girdiğiniz Sayıların Toplamı = "+toplam);
+            Console.WriteLine("Girdiğiniz Sayıların Ortalaması = "+((double)toplam/adet));
+        }
+    }
+}
---
+++ 20230812_Metodlar_Ornek2/20230812_Metodlar_Ornek2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20230812_Metodlar_Ornek2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kullanıcıdan kaç adet sayı girmek istediğini talep edelim
+            //Talep edilen Sayıların toplamını ve ortalamasını ekrana yazdıralım
+            Console.Write("Kaç Adet Sayı Girmek İstersiniz ");
+            int adet=int.Parse(Console.ReadLine());
+
+            int[] sayilarDizisi = new int[adet];
+            for (int i = 0; i < sayilarDizisi.Length; i++)
+            {
+                Console.Write((i+1)+".Sayıyı Girin");
+                sayilarDizisi[i]=int.Parse(Console.ReadLine());
+            }
+
+            Metodlar metodlar=new Metodlar();
+            int toplam2 = metodlar.ToplamaYaptir(sayilarDizisi);
+
+            metodlar.ToplamiVeOrtalamayiYazdir(toplam2,sayilarDizisi.Length);
+
+            Console.ReadKey();
+
+        }
+    }
+}
